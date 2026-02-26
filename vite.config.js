48851dec3d3ca8ce305df8f99799ea754d922f77
darkangel152 <122563134+darkangel152@users.noreply.github.com>
2026-02-26 15:01:50 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+  root: ".",        // project root
+  publicDir: "public",
   build: {
-    outDir: "dist"
-  }
+    outDir: "dist",
+  },
 });
